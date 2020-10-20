398efd685470ada412aa431b3f5369237c05dd6c
gc200295036 <nickbrazda95@hotmail.com>
2020-10-20 18:34:03 -0400

    first commit

---
+++ functions/hello.js
@@ -0,0 +1,24 @@
+// functions/hello.js
+let votesArray = [0, 0, 0, 0];
+
+exports.handler = async (event) => {
+  const v = event.queryStringParameters.vote;
+  if (v === 'a') {
+    votesArray[0] += 1;
+  } else if (v === 'b') {
+    votesArray[1] += 1;
+  } else if (v === 'c') {
+    votesArray[2] += 1;
+  } else if (v === 'd') {
+    votesArray[3] += 1;
+  } else if (v === 'r') {
+    votesArray = [0, 0, 0, 0];
+  }
+  console.log(JSON.stringify(votesArray));
+
+  return {
+    statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify(votesArray),
+  };
+};
